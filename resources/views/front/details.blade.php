1f356299b298206d99cb0e9b0a0551989bcd1b4d
Ebuild-git <achref.hamouda@e-build.tn>
2024-02-18 00:29:03 +0100

    admin

--- resources/views/front/details.blade.php
+++ resources/views/front/details.blade.php
@@ -84,20 +84,6 @@
                             </div>
                         </div>
 
-                        <!-- Start Post Pagination -->
-                        <div class="post-pagi-area">
-                            <a href="#">
-                                <i class="fas fa-angle-double-left"></i> Previus Post
-                                <h5>Hello World</h5>
-                            </a>
-                            <a href="#">
-                                Next Post <i class="fas fa-angle-double-right"></i>
-                                <h5>The earth brown</h5>
-                            </a>
-                        </div>
-                        <!-- End Post Pagination -->
-
-
                     </div>
                 </div>
             </div>
